6cd62a1e44d3bd0bf607a9d5d9e6546593bb6cf9
hazelnutcloud <ha5anmukhli50@gmail.com>
2023-03-17 12:57:58 +0800

    update script

--- test.ts
+++ test.ts
@@ -1,3 +1,8 @@
+import { join } from "https://deno.land/std@0.179.0/path/mod.ts";
+
 if (import.meta.main) {
-  console.log(Deno.cwd());
+  const path = Deno.args[0];
+  console.log(join(Deno.cwd(), path));
+
+  await import(join(Deno.cwd(), path));
 }
