59c9246cedbdf0cae37b3dd85dd8d3a48cda8a54
hazelnutcloud <ha5anmukhli50@gmail.com>
2023-03-17 16:13:24 +0800

    pls werk

--- test.ts
+++ test.ts
@@ -1,10 +1,6 @@
 import { join } from "https://deno.land/std@0.179.0/path/mod.ts";
 
 if (import.meta.main) {
-  const dir = Deno.env.get("DENO_DIR");
-  console.log(dir);
   const path = Deno.args[0];
-  console.log(join(Deno.cwd(), path));
-  Deno.env.set("DENO_DIR", "/tmp");
-  await import(join(Deno.cwd(), path));
+  await import(`file://${join(Deno.cwd(), path)}`);
 }
